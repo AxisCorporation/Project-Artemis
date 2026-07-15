adcc6f0b4aa4d73157c81f7380bef2a729c0ff19
Zain Farhan <zainfarhan.work@gmail.com>
2026-07-16 02:31:16 +0500

    Main Menu

---
+++ Scripts/Utils/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Prototype.tscn")
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
